59395aa341021fe32e853d322c8e46f2704960ff
Marcos Guilherme Soeres Pereira <202401355313@alunos.estacio.br>
2024-09-11 22:21:59 -0300

    Version 1.2 Delete

--- Structs/game_catalog.c
+++ Structs/game_catalog.c
@@ -3,15 +3,14 @@
 //
 
 #include <stdio.h>
-#include <string.h>
+#include "string.h"
 
 typedef struct
 {
     int day;
     int month;
     int year;
-
-}data;
+} data;
 
 typedef struct
 
@@ -21,13 +20,12 @@ typedef struct
     data releasedata;
     float notaAva;
     int temjogo;
+} Game;
 
-}Game;
 
-
-void setcatalogo(Game *jogos,int capacidade)
+void setcatalogo(Game* jogos, int capacidade)
 {
-    for (int i = 0; i <capacidade; i++)
+    for (int i = 0; i < capacidade; i++)
     {
         strcpy(jogos[i].nome, "");
         strcpy(jogos[i].plataforma, "");
@@ -39,7 +37,7 @@ void setcatalogo(Game *jogos,int capacidade)
     }
 }
 
-Game addGame(const char *nome, const char *plataforma,int day,int month,int ano,float nota)
+Game addGame(const char* nome, const char* plataforma, int day, int month, int ano, float nota)
 {
     Game game;
     strcpy(game.nome, nome);
@@ -53,7 +51,7 @@ Game addGame(const char *nome, const char *plataforma,int day,int month,int ano,
     return game;
 }
 
-void addcatalogo(Game *vetorjogos,int tamanhovetor,Game addgame,int *posicao)
+void addcatalogo(Game* vetorjogos, int tamanhovetor, Game addgame, int* posicao)
 {
     int cont = 0;
     for (int i = 0; i < tamanhovetor; i++)
@@ -72,11 +70,10 @@ void addcatalogo(Game *vetorjogos,int tamanhovetor,Game addgame,int *posicao)
     {
         vetorjogos[*posicao] = addgame;
         *posicao = *posicao + 1;
-
     }
 }
 
-void printdata(int day,int month,int year)
+void printdata(int day, int month, int year)
 {
     int formatint = 0;
 
@@ -84,30 +81,29 @@ void printdata(int day,int month,int year)
 
     if (day < 10)
     {
-        printf("%d",formatint);
+        printf("%d", formatint);
     }
 
-    printf("%d/",day);
+    printf("%d/", day);
 
-    if  (month < 10)
+    if (month < 10)
     {
-        printf("%d",formatint);
+        printf("%d", formatint);
     }
 
-    printf("%d/%d\n",month,year);
-
+    printf("%d/%d\n", month, year);
 }
 
-void imprimejogo(Game *jogo)
+void imprimejogo(Game* jogo)
 {
     printf("Nome: %s\n", jogo->nome);
     printf("Plataforma: %s\n", jogo->plataforma);
-    printdata(jogo->releasedata.day,jogo->releasedata.month,jogo->releasedata.year);
+    printdata(jogo->releasedata.day, jogo->releasedata.month, jogo->releasedata.year);
     printf("Nota: %.1f\n", jogo->notaAva);
     printf("-------------------------------------\n");
 }
 
-void buscaPlataforma(Game *jogos,int tamanhovetor)
+void buscaPlataforma(Game* jogos, int tamanhovetor)
 {
     char plataforma[30];
     printf("\nBusque jogos pela plataforma\n>: ");
@@ -116,28 +112,28 @@ void buscaPlataforma(Game *jogos,int tamanhovetor)
 
     int cond = 0;
     while (cond == 0)
+    {
+        for (int i = 0; i < tamanhovetor; i++)
         {
-        for (int i = 0;i < tamanhovetor;i++)
+            if (strcmp(jogos[i].plataforma, plataforma) == 0)
             {
-                if (strcmp(jogos[i].plataforma, plataforma) == 0)
-                {
-                    imprimejogo(&jogos[i]);
-                    cond = 1;
-                }
-            }
-        if (cond != 1)
-            {
-                printf("Não encontrada, tente denovo\n");
-                printf("\nBusque jogos pela plataforma\n>: ");
-                scanf("%s", plataforma);
-                printf("-------------------------------------\n");
+                imprimejogo(&jogos[i]);
+                cond = 1;
             }
         }
+        if (cond != 1)
+        {
+            printf("Não encontrada, tente denovo\n");
+            printf("\nBusque jogos pela plataforma\n>: ");
+            scanf("%s", plataforma);
+            printf("-------------------------------------\n");
+        }
     }
+}
 
-void imprimetodosjogos(Game *jogos,int tamanhovetor)
+void imprimetodosjogos(Game* jogos, int tamanhovetor)
 {
-    for(int i=0;i<tamanhovetor;i++)
+    for (int i = 0; i < tamanhovetor; i++)
     {
         if (jogos[i].releasedata.year != 0)
         {
@@ -147,17 +143,13 @@ void imprimetodosjogos(Game *jogos,int tamanhovetor)
         {
             printf("Sem Jogo adicionado na posição: %d\n", i + 1);
             printf("-------------------------------------\n");
-
         }
     }
 }
 
-void delete_game(Game *jogos, int tamanhovetor)
+void delete_game(Game* jogos, int tamanhovetor)
 {
-    char s[1] = {"s"};
-    char confirmation[1] = {"z"};
-
-    fflush(stdin);
+    char confirmation[1] = {"s"};
 
     printf("Deseja deletar algum jogo?\n");
     printf("s ou n? ");
@@ -167,15 +159,19 @@ void delete_game(Game *jogos, int tamanhovetor)
 
     while (end == 0)
     {
-        fflush(stdin);
         scanf("%s", confirmation);
 
-        if (strcmp(s, confirmation) == 0)
+        if ((strcmp(confirmation, "n") != 0) && (strcmp(confirmation, "s") != 0))
         {
+            printf("\nTecla invalida!\n\n");
+            printf("Deseja deletar algum jogo?\n");
+            printf("s ou n? ");
+        }
 
+        if (strcmp(confirmation, "s") == 0)
+        {
             char name[30];
             printf("Digite o nome do jogo, para deletar: ");
-            fflush(stdin);
             scanf(" %[^\n]", name);
 
             while (end2 == 0)
@@ -185,23 +181,27 @@ void delete_game(Game *jogos, int tamanhovetor)
                     if (strcmp(jogos[i].nome, name) == 0)
                     {
                         setcatalogo(&jogos[i], 1);
-                        end2 = 1;
                         printf("\n");
-                        printf("Jogo: %s foi removido. \n\n", name);
-
+                        printf("Jogo: %s foi removido. \n", name);
+                        printf("\nDeseja deletar outro jogo?\n");
+                        printf("s ou n? ");
                     }
+                    end2 = 1;
                 }
                 if (end2 == 0)
                 {
                     printf("\nDigite jogo valido!\n\n");
                     printf("Digite o nome do jogo, para deletar: ");
-                    fflush(stdin);
                     scanf(" %[^\n]", name);
                 }
             }
         }
-        end = 1;
+        if (strcmp(confirmation, "n") == 0)
+        {
+            end = 1;
+        }
     }
+    printf("\n");
 }
 
 int main(void)
@@ -212,21 +212,22 @@ int main(void)
     int* PosicaodeSubistituicao = &posi;
     int capacidade_total = sizeof(jogos) / sizeof(jogos[0]);
 
-    setcatalogo(jogos,capacidade_total);
+    setcatalogo(jogos, capacidade_total);
 
-    addcatalogo(jogos,capacidade_total,addGame("Valorant","Pc",10,03,2017,8), PosicaodeSubistituicao);
-    addcatalogo(jogos,capacidade_total,addGame("Doom","Pc",31,25,2017,8), PosicaodeSubistituicao);
-    addcatalogo(jogos,capacidade_total,addGame("Crash Bandicoot","Playstation",1,4,2017,8),PosicaodeSubistituicao);
-    addcatalogo(jogos,capacidade_total,addGame("Resident Evil","Playstation",23,8,2017,8),PosicaodeSubistituicao);
-    addcatalogo(jogos,capacidade_total,addGame("Mario kart","Nintendo",2,2,1998,8),PosicaodeSubistituicao);
-    addcatalogo(jogos,capacidade_total,addGame("123","Playstation",32,04,2017,8),PosicaodeSubistituicao);
-    addcatalogo(jogos,capacidade_total,addGame("345","Playstation",04,05,2017,8),PosicaodeSubistituicao);
-    addcatalogo(jogos,capacidade_total,addGame("678","Playstation",1,12,2017,8),PosicaodeSubistituicao);
+    addcatalogo(jogos, capacidade_total, addGame("Valorant", "Pc", 10, 03, 2017, 8), PosicaodeSubistituicao);
+    addcatalogo(jogos, capacidade_total, addGame("Doom", "Pc", 31, 25, 2017, 8), PosicaodeSubistituicao);
+    addcatalogo(jogos, capacidade_total, addGame("Crash Bandicoot", "Playstation", 1, 4, 2017, 8),
+                PosicaodeSubistituicao);
+    addcatalogo(jogos, capacidade_total, addGame("Resident Evil", "Playstation", 23, 8, 2017, 8),
+                PosicaodeSubistituicao);
+    addcatalogo(jogos, capacidade_total, addGame("Mario kart", "Nintendo", 2, 2, 1998, 8), PosicaodeSubistituicao);
+    addcatalogo(jogos, capacidade_total, addGame("123", "Playstation", 32, 04, 2017, 8), PosicaodeSubistituicao);
+    addcatalogo(jogos, capacidade_total, addGame("345", "Playstation", 04, 05, 2017, 8), PosicaodeSubistituicao);
+    addcatalogo(jogos, capacidade_total, addGame("678", "Playstation", 1, 12, 2017, 8), PosicaodeSubistituicao);
 
-    imprimetodosjogos(jogos,capacidade_total);
-    delete_game(jogos,capacidade_total);
-    imprimetodosjogos(jogos,capacidade_total);
-    buscaPlataforma(jogos,capacidade_total);
+    imprimetodosjogos(jogos, capacidade_total);
+    delete_game(jogos, capacidade_total);
+    imprimetodosjogos(jogos, capacidade_total);
 
     return 0;
 }
--- Structs/linked_lists.c
+++ Structs/linked_lists.c
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 typedef struct
@@ -14,10 +15,18 @@ typedef struct
 
 }Game;
 
+void aloc_memo(*list, size_list)
+{
+    for (int i = 0; i < size_list; i++)
+    {
+        size_list[i] = malloc(sizeof(Game))
+    }
+}
 int main(void)
 {
     Game linked_List[3];
-	
+    size_list = (sizeof(linked_list) / sizeof(linked_list[0]));
+
 
     return 0;
 }
